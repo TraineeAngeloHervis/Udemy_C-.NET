5826e85236a4cc0bdc5f25223216d8c41ba4effc
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-05-14 20:42:36 -0300

    Aprendendo a usar o NuGet

--- Aula_Estrutura_Basica/Program.cs
+++ Aula_Estrutura_Basica/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using FluentColorConsole;
+
+Console.WriteLine("Hello, World!");
 
 var showMessage = new ShowMessage();
 showMessage.Escrever();
+
+var textLine = ColorConsole.WithBlueText;
+textLine.WriteLine("Aprendendo a usar o NuGet");
