2237604a98fd04bcbb1142a3f4b5fa4dc65574f3
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-05-16 09:03:05 -0300

    API Funciona mesmo

--- Aula_Web/Program.cs
+++ Aula_Web/Program.cs
@@ -2,5 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Aprendendo a usar API");
+app.MapPost(
+    "/user",
+    () =>
+        new
+        {
+            Name = "Ângelo Hervis",
+            Age = 18,
+            CEP = 83810000,
+            Email = "Angelo.hervis@gmail.com"
+        }
+);
+app.MapGet(
+    "/AddHeader",
+    (HttpResponse response) => {
+        response.Headers.Add("Testando...", "API funciona mesmo.");
+        return "Funcionou";
+});
 
 app.Run();
--- Aula_Web/obj/Debug/net8.0/Aula_Web.AssemblyInfo.cs
+++ Aula_Web/obj/Debug/net8.0/Aula_Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be23978bbff8cf0a1ade292d7ace24d49dc8da01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc720e42f5cb77313fc82af04dc94e9015dbe8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
